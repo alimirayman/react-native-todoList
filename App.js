f5ed5594c0c2c92adde438db0da6e15c5545b2d3
Mir Ayman Ali <alimirayman@outlook.com>
2020-03-30 09:51:51 +0600

    basic app with text input and listed items

--- App.js
+++ App.js
@@ -1,21 +1,71 @@
-import React, {useState} from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import React, { useState } from 'react'
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  FlatList
+} from 'react-native'
 
 export default function App() {
-  const [outputText, setOutputText]= useState('Open up App.js to start working on your app!')
+  const [enteredGoal, setEnteredGoal] = useState('')
+  const [courseGoals, setCourseGoals] = useState([])
+
+  const goalInputHandler = enteredText => {
+    setEnteredGoal(enteredText)
+  }
+  const addGoalHandler = () => {
+    setCourseGoals(courseGoals => [
+      ...courseGoals,
+      { id: Math.random().toString(), value: enteredGoal }
+    ])
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>{outputText}</Text>
-      <Button title="Change Text"  onPress={() => setOutputText('Titlte Changed')} />
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Course Goal"
+          style={styles.input}
+          onChangeText={goalInputHandler}
+          value={enteredGoal}
+        />
+        <Button title="Add" onPress={addGoalHandler} />
+      </View>
+      <FlatList
+        keyExtractor={(item, index) => item.id}
+        data={courseGoals}
+        renderItem={itemData => (
+          <View style={styles.listItem}>
+            <Text>{itemData.item.value}</Text>
+          </View>
+        )}
+      />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  screen: {
+    padding: 50
   },
-});
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  input: {
+    width: '80%',
+    borderColor: 'black',
+    borderWidth: 1,
+    padding: 10
+  },
+  listItem: {
+    padding: 10,
+    backgroundColor: '#ddd',
+    borderColor: '#222',
+    borderWidth: 1,
+    marginVertical: 10
+  }
+})
